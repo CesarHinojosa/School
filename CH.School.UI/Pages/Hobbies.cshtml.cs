81c0c0a5c1116a42de34fb24222ba4afea99d9e8
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2023-08-31 13:39:31 -0500

    Added Hobbies and CSS modification

---
+++ CH.School.UI/Pages/Hobbies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CH.School.UI.Pages
+{
+    public class HobbiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
